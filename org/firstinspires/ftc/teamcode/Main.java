7073743634101d95758c2785bab9845f0e99b512
Emily <27epan@ga.usmk12.org>
2024-09-24 16:00:08 -0500

    created classes for subsystems + other stuff we probably need

---
+++ org/firstinspires/ftc/teamcode/Main.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode;
+
+class StarterClass {
+    //comment
+}
---
+++ org/firstinspires/ftc/teamcode/Subsystems/ArmSubsystem.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+public class ArmSubsystem {
+    
+}
